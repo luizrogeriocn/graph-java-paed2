b379fd5ef7c8327b5978dec6db9c249fc5426340
luizrogeriocn <luizrogeriocn@gmail.com>
2013-06-07 06:36:57 -0300

    consertando tudo

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,67 +1,139 @@
+import java.util.Collections;
 import java.util.Vector;
 
 public class MaxHeap {
 	
-	protected Vector<Vertex> vertices;
+	public static Vector<Node> nodes;
+	public static int size;
 	
 	public MaxHeap(){
-		this.vertices = new Vector<Vertex>();
-		this.vertices.add(null);
+		nodes = new Vector<Node>();
+		nodes.add(null);
+		size = 0;
 	}
 
-	public void insert(int id, int chave){
-		Vertex vertex = new Vertex(id, chave);
-		this.vertices.add(vertex);
-		insertHelper();
-		System.out.println("-");
+	public void insert(int id, int cost){
+		Node node = new Node(id, cost);
+		nodes.add(node);
+		size++;
+		goUp(size);
 	}
 	
 	public void extract(){
-		 Vertex result = this.vertices.get(1); 
+		Node node = nodes.get(1);
+		Collections.swap(nodes, 1, size);
+		nodes.remove(size);
+		size--;
+		goDown(1);
 	}
 	
-	public void decrease(){
-		
+	public void decrease(int id, int cost){
+		int index = indexOfId(id);
+		nodes.get(index).cost = cost;
+		goDown(index);
 	}
 	
-	public void increase(){
-		
+	public void increase(int id, int cost){
+		int index = indexOfId(id);
+		nodes.get(index).cost = cost;
+		goUp(index);
 	}
 	
-	public void maxHeapfy(){
-		
-	}
-	
-	public void insertHelper(){
-		int currentPosition = vertices.size() -1;
-		Vertex auxVertex;
-		
-		while( (currentPosition > 1) && (vertices.get(currentPosition).currentCost > vertices.get(currentPosition/2).currentCost) ){
-			auxVertex = vertices.get(currentPosition);
-			vertices.set(currentPosition, vertices.get(currentPosition/2));
-			vertices.set(currentPosition/2, auxVertex);
-			currentPosition = currentPosition/2;
+	public void goUp(int index){
+		int currentPos = index;
+		while(currentPos > 1 && costAt(currentPos) > costAt(currentPos/2)){
+			Collections.swap(nodes, currentPos, currentPos/2);
+			currentPos = currentPos/2;
 		}
 	}
+	
+	public void goDown(int index){
+		int currentPos = index;
+		while(currentPos <= size/2){
+			if(costAt(greaterChild(currentPos)) > costAt(currentPos)){
+				Collections.swap(nodes, currentPos, greaterChild(currentPos));
+			}
+			currentPos = greaterChild(currentPos);
+		}	
+	}
+	
+	public static int rightChildCost(int index){
+		if(size >= index*2 +1)
+			return costAt(index*2 +1);
+		else
+			return -1;
+	}
+	
+	public static int leftChildCost(int index){
+		if(size >= index*2)
+			return costAt(index*2);
+		else
+			return -1;
+	}
+	
+	public static boolean hasChild(int index){
+		if(size >= index*2)
+			return true;
+		else
+			return false;
+	}
+	
+	public static int greaterChild(int index){
+		int a = rightChildCost(index);
+		int b = leftChildCost(index);
+		if(a > b){
+			return index*2+1;}
+		else{
+			return index*2;}
+	}
+	
+	public int father(int index){
+		return index/2;
+	}
+	
+	public static int costAt(int index){
+		return nodes.get(index).cost;
+	}
+	
+	public void swap(int a, int b){
+		Node aux = nodes.get(a);
+		nodes.add(a, nodes.get(b));
+		nodes.add(b, aux);
+	}
+	
+	public int indexOfId(int id){
+		int res = -1;
+		for(int i = 1; i < size; i++){
+			if(id == nodes.get(i).id)
+				res = i;
+		}
+		return res;
+	}
+	
+	public void printHeap(){
+		for(int i = 1; i < size; i++){
+			System.out.println(costAt(i));
+			System.out.println("oi");
+		}
+	}
+	
+
 	
 	public static void main(String[] args){
 		MaxHeap hp = new MaxHeap();
+		
 		hp.insert(1, 100);
-
 		hp.insert(2, 300);
-
 		hp.insert(3, 250);
-
 		hp.insert(4, 1000);
-
 		hp.insert(5, 500);
 		hp.insert(6, 254);
 		hp.insert(7, 15);
-		hp.insert(10, 14);
-		for(int i = 1; i < hp.vertices.size(); i++){
-			System.out.println(hp.vertices.get(i).currentCost);
+		//hp.decrease(5, 302);
+		hp.extract();
+		for(int i = 1; i <= size; i++){
+			System.out.println(hp.nodes.get(i).cost);
 		}
-
 	}
 	
 }
--- Node.java
+++ Node.java
@@ -2,9 +2,15 @@
 public class Node {
 	public int predecessor;
 	public int cost;
+	public int id;
 	
 	public Node(int i) {
 		this.predecessor = i ;
 		this.cost = 0;
+	}
+	
+	public Node(int id, int cost) {
+		this.id = id ;
+		this.cost = cost;
 	}
 }
